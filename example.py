d2b0ffa9eb66c91f183dd173b0852278f7e4e8f9
NurdosKanalbek <157092936+NurdosKanalbek@users.noreply.github.com>
2024-01-26 12:32:25 +0600

    Add files via upload

---
+++ example.py
@@ -0,0 +1,51 @@
+if 5 > 2:
+    print("Five is greater than two!")
+
+#This is comment
+print("Hello world")
+
+#print("Hello world")
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = 4 #x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+x = "Python is awesome"
+print(x)
+
+x = "awesome"
+def myFunc():
+    print("Python is " + x)
+
+myFunc()
+
+x = 1 #int
+y = 2.8 #float
+z = 1j #complex
+
+x = int(1) # x will be 1
+y = int(2.8) #y will be 2
+z = int("3") #z will be 3
+
+x = float(1) # x will be 1.0
+
+a = "Hello"
+print(a)
+
+a == """Lorem ipsum
+aaaaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaaa"""
+print(a)
+
